e7957627c7a25fcc6462ce77498fe6252d009159
Nemanicka <Nemanicka@gmail.com>
2015-05-20 13:15:43 +0000

    added some streaming twitter api

--- lib/searchers/twitter.rb
+++ lib/searchers/twitter.rb
@@ -9,34 +9,45 @@ require "twitter"
 class FoursquareSearcher
 
     def initialize( attributes = {} )
-        location = [ 50.45025, 30.523889 ]
+        geocode = "50.45025,30.523889, 20km"
     
-        curb = Curl::Easy.new
-        constructUrl( location );
+        @client = Twitter::Streaming::Client.new do |config|   
+            config.consumer_key = "RtZzMs8aNhEsmCs1uqpUc7kbm"   
+            config.consumer_secret = "6IQnIVDRNKKRK2u01DFxBtUT8wrGeu6hxUAWORz4jeNDF3E1fw"   
+            config.access_token = "808599781-6161951XO8DVGE5HzdLvU6C4jb754UVhqRg0GX3R"   
+            config.access_token_secret = "DiW3zMENZ2VuAPkdSPwnhk8zLBrcraEYccAIQw4kgqtpH" 
+        end
+#        getRESTTweets( geocode );
+
+        getStreamTweets( geocode )
+
 #        puts curb.url
 #        curb.http_get()
 #        parsed_answer = JSON.parse( curb.body_str );
     end
 
-    def constructUrl ( location )
-        client = Twitter::REST::Client.new do |config|   
-        config.consumer_key = "RtZzMs8aNhEsmCs1uqpUc7kbm"   
-        config.consumer_secret = "6IQnIVDRNKKRK2u01DFxBtUT8wrGeu6hxUAWORz4jeNDF3E1fw"   
-        config.access_token = "808599781-6161951XO8DVGE5HzdLvU6C4jb754UVhqRg0GX3R"   
-        config.access_token_secret = "DiW3zMENZ2VuAPkdSPwnhk8zLBrcraEYccAIQw4kgqtpH" 
-    end
-        geocode = {:lat => 50.45025, :long => 30.523889, :radius => "20km" }
-        lang = {:lang => "en"}
+    def getRESTTweets ( geocode )
         
-        obj = client.search( "", { :geocode => "50.45025,30.523889,40km"} )
-#        obj = client.geo_search( {:lat => 50.45025, :long => 30.523889} )
+        clientREST = Twitter::REST::Client.new( @client.credentials )
+        
+#        clientREST = @client
+        
+        obj = clientREST.search( "", { :geocode => geocode } )
         objh = obj.to_h;
         counter = 0
         objh[:statuses].each {|x| puts x[:text]; counter+=1   }
      
-   puts counter
-#        puts objh[:statuses]
-        
+        puts counter 
+    end
+
+    def getStreamTweets( geocode )
+#        clientStream = Twitter::Streaming::Client.new( @client.credentials )
+#        puts @client.credentials
+#:locations => geocode
+        @client.filter( { :track => "lol",  :location => geocode } ) do |x|
+#        @client.filter( :track => "lal,lol" ) do |x|
+            puts x.text if x.is_a?(Twitter::Tweet)
+        end
     end
 
 
