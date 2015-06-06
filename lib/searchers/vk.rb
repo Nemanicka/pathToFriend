afaa3eb3f5a8d27dd2e01207455614bbb352d4b2
EC2 Default User <ec2-user@ip-172-31-26-189.eu-central-1.compute.internal>
2015-06-06 14:43:03 +0000

    implemented cross search

--- lib/searchers/vk.rb
+++ lib/searchers/vk.rb
@@ -17,60 +17,148 @@ require 'eventmachine'
 require 'em-http-request'
 
 
+
+class DSQueue
+    @q
+
+    def initialize( attributes = {} )
+        @q = Array.new
+    end
+
+    def push(arg)
+        @q.push(arg)
+    end
+
+    def pop
+        res = @q.first
+        @q.delete_at(0)
+        return res
+    end
+
+    def front
+        return @q.first
+    end
+
+    def to_s
+        return @q.to_s
+    end
+
+    def size
+        return @q.size
+    end
+
+end
+
+
+
 class FoursquareSearcher
     @maxThreadNum
 
-    @leftUsersHash
-    @rightUsersHash
+    @usersHash
+    @rUserHash
+    @lUserHash
     @leftUsers   
     @rightUsers
+    @result    
+    @finish1
+    @finish2
+    @q1
+    @q2
+    @found
 
     def initialize( attributes = {} )
-        @leftUsersHash = Bitset.new( 500000000 );
-        @rightUsersHash = Bitset.new( 500000000 );
-        @leftUsers = Array.new
-        @rightUsers = Array.new
-        @maxThreadNum = 3
+  
 
-        @leftUsers << 1;
-        @rightUsers << 7777777;
-        puts "start"
-        t1 = Time.now
-        execEM();
-        t2 = Time.now
-        t = t2 - t1
-        puts t
-    end
 
-    def execEMulti
-        EventMachine.run do   
-            multi = EventMachine::MultiRequest.new    
+        @result = Array.new
+        @lUserHash = Bitset.new 1000000000 
+        @rUserHash = Bitset.new 1000000000 
+        @fetched = Bitset.new 10000000 
+        @finish1 = false        
+        @finish2 = false        
+
+
+        @q1 = DSQueue.new
+        @q1.push( [1] )
+        
+        @q2 = DSQueue.new
+        @q2.push( [ 77777] )
+       
+        #@leftStack = Array.new
+        #@rightStack = Array.new
+        
+
+
+        f1 = Fiber.new do |f|
+        
             
+            while @finish1 == false do 
+                
+            
+            puts "f1"            
+            fetchFriends( @q1, @lUserHash, @finish  )
+            f.transfer f1
+            Fiber.yield
+            end
+        end
+        
+        f2 = Fiber.new do |f|
+        
+        
+            while @finish2 == false do
+            
+            puts "f2"            
+            fetchFriends( @q2, @rUserHash, @finish )
+            f.transfer f2
+            Fiber.yield
+            end
+        end
+        
+        f1.resume f2
+        
+#            fetchFriends( @q1, @lUserHash )
+#        @finish = false        
+#            fetchFriends( @q2, @rUserHash )
+
+        #q.push({2=>1})
+        #q.push(2=>3)
+        #q.push(2=>4)
+        #puts q
+        #q.push(5=>6)
+        #puts q
+        #a = q.pop()
+        #puts "a =   #{a}"
+        #puts q
+        
+    end
     
-        #    multi.add (:vk1, EventMachine::HttpRequest.new('https://api.vk.com/method/friends.get').post) :body => {:user_id => 1 }
-      #      multi.add (:vk2, EventMachine::HttpRequest.new('https://api.vk.com/method/friends.get').post) :body => {:user_id => 77777 } 
-            multi.callback do     
-                puts multi.responses[:callback]     
-                puts multi.responses[:errback]     
-                EventMachine.stop   
-            end 
-        end 
+    def test( method )
+        puts "test"
+        method.call( )
     end
 
-    def execEM
-            total = 0
-            succeed = 0
-            failed = 0
+    def tcallback
+        puts "works"
+    end
+
+    def fetchFriends( queue, mybitset, opbitset, finish, stack )
+        total = 0
+        succeed = 0
+        failed = 0
         EM.run do   
-           # http = []
             counter = 0
-            1.upto(100) do |x|
+            while( finish == false ) do
                 counter+=1
-          #  1.upto(5) do |x|
-           #     puts x
-                http = ( EM::HttpRequest.new('https://api.vk.com/method/friends.get', :connect_timeout => 2, :inactivity_timeout => 10 ).post :body => {:user_id => x } )
-         #   end
-        #    http.each do |x|
+                userStack = queue.pop()
+                    puts "first elem = #{userStack}, size = #{queue.size}" 
+                id = userStack[0]
+                
+                next if @fetched[id] == 1     
+                
+                
+ 
+                http = ( EM::HttpRequest.new('https://api.vk.com/method/friends.get', :connect_timeout => 10, :inactivity_timeout => 10 ).post :body => {:user_id => id } )
+                finish = true if( found == true and userStack.size != queue.front().size ) 
                 http.errback { 
                     failed+=1
                     total +=1
@@ -78,25 +166,45 @@ class FoursquareSearcher
                     EM.stop if counter == 1
                     counter-=1;
                 } 
-       #     end
-     #       http.each do |x|
-                http.callback { 
+                http.callback {
                     succeed+=1
                     total+=1
-                    p http.response;
-                    puts counter
+                    a = JSON.parse(http.response)["response"]
+                    userStack.unshift( id )
+                    queue.push( userStack )
+                    @fetched[id] = 1
+                    mybitset[id] = 1
+                    if ( opbitset[id] == 1 ) do
+                        found = true
+                    end
+                    a.each { |x| bitset[x] = 1  }
                     EM.stop if counter == 1
                     counter-=1;
-                } 
-      #      end
-     #      end  
-           
+                }
            end
-        end   
+        end
         puts "succeed = #{succeed}/#{total}"  
         puts "failed = #{failed}/#{total}"  
         puts "Done."
     end
+    
+    def fetchFriendsErrCallback
+        
+    end
+
+    def fetchFriendsCallback
+   #     @succeed+=1
+         puts "callback lol"
+   #     @total+=1
+   #     p self.response;
+   #     puts @counter
+   #     EM.stop if @counter == 1
+   #                @counter-=1;
+    
+    end
 end
+
+
+
  test = FoursquareSearcher.new
 
